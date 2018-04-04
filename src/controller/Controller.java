37bc275007d0c4d2560c878dc2b5f81791352996
mmughrabi <mydo.mughrabi@gmail.com>
2018-04-04 13:56:35 +0200

    MVC Backbone

---
+++ src/controller/Controller.java
@@ -0,0 +1,15 @@
+package controller;
+
+import model.Model;
+import view.View;
+
+public class Controller {
+	Model model;
+	View view;
+	
+	public Controller(Model model, View view)
+	{
+		this.model = model;
+		this.view = view;
+	}
+}
