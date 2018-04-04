92586c4645d1d5f630129bc7352968c81855b0e9
mmughrabi <mydo.mughrabi@gmail.com>
2018-04-04 15:40:50 +0200

    Open a black window

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,18 @@ import controller.Controller;
 import model.Model;
 import view.View;
 
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
 public class Main {
 
 	public static void main(String[] args) {
 		Model model = new Model();
 		View view = new View();
 		Controller controller = new Controller(model, view);
-
 	}
 
 }
--- src/view/View.java
+++ src/view/View.java
@@ -1,4 +1,52 @@
 package view;
 
+import static org.lwjgl.glfw.GLFW.GLFW_FALSE;
+import static org.lwjgl.glfw.GLFW.GLFW_VISIBLE;
+import static org.lwjgl.glfw.GLFW.glfwCreateWindow;
+import static org.lwjgl.glfw.GLFW.glfwGetPrimaryMonitor;
+import static org.lwjgl.glfw.GLFW.glfwGetVideoMode;
+import static org.lwjgl.glfw.GLFW.glfwInit;
+import static org.lwjgl.glfw.GLFW.glfwMakeContextCurrent;
+import static org.lwjgl.glfw.GLFW.glfwPollEvents;
+import static org.lwjgl.glfw.GLFW.glfwSetWindowPos;
+import static org.lwjgl.glfw.GLFW.glfwShowWindow;
+import static org.lwjgl.glfw.GLFW.glfwSwapBuffers;
+import static org.lwjgl.glfw.GLFW.glfwTerminate;
+import static org.lwjgl.glfw.GLFW.glfwWindowHint;
+import static org.lwjgl.glfw.GLFW.glfwWindowShouldClose;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.glClear;
+
+import org.lwjgl.glfw.GLFWVidMode;
+import org.lwjgl.opengl.GL;
+
 public class View {
+	
+	public View()
+	{
+		if(!glfwInit())
+		{
+			throw new IllegalStateException("not inited");
+		}
+		
+		glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+		long window = glfwCreateWindow(640, 480, "hello", 0, 0);
+		if(window==0){throw new IllegalStateException("not created");}
+		glfwMakeContextCurrent(window);
+		GL.createCapabilities();
+		GLFWVidMode videoMode = glfwGetVideoMode(glfwGetPrimaryMonitor());
+		glfwSetWindowPos(window, (videoMode.width()-640)/2, (videoMode.height()-480)/2);
+		glfwShowWindow(window);
+		
+		while (!glfwWindowShouldClose(window))
+		{
+			glfwPollEvents();
+			
+			glClear(GL_COLOR_BUFFER_BIT);
+			
+			glfwSwapBuffers(window);
+		}
+		
+		glfwTerminate();
+	}
 }
